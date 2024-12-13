ead2f8e9c7b572e9a97807c75c156a74e1da3816
Usama <kusamakhan1234@gmail.com>
2024-12-14 03:43:08 +0500

    Delete src/components/CheckOut directory

--- src/components/CheckOut/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as BillingDetails } from './BillingDetails';
-export { default as OrderSummary } from './OrderSummary';
-export { default as PaymentMethod } from './PaymentMethod'; 
